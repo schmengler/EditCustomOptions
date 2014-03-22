a0f523c8a65ec4e6e0671b7b7dc13aa214ecc230
Fabian Schmengler <fschmengler@sgh-it.eu>
2014-03-22 15:53:31 +0100

    Refactor: Add popup block via observer to additional product information.

---
+++ app/code/local/SSE/EditCustomOptions/Block/Popup.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Popup template
+ * 
+ * @author Fabian Schmengler
+ *
+ */
+class SSE_EditCustomOptions_Block_Popup extends Mage_Core_Block_Template
+{
+	const ALIAS = 'editcustomoptions/popup';
+}
---
+++ app/code/local/SSE/EditCustomOptions/Model/Observer.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Observer
+ * 
+ * @author Fabian Schmengler
+ */
+class SSE_EditCustomOptions_Model_Observer
+{
+	const POPUP_ALIAS_IN_LAYOUT = 'custom_options_popup';
+	/**
+	 * Adds popup block with custom option form to be used by item renderer in order overview
+	 * 
+	 * @param Varien_Event_Observer $observer
+	 * @see event core_block_abstract_to_html_before
+	 */
+	public function addPopupBlockToLayout(Varien_Event_Observer $observer)
+	{
+		$block = $observer->getBlock();
+		if ($block instanceof Mage_Sales_Block_Order_Item_Renderer_Default) {
+			/* @var $container Mage_Core_Block_Text_List */
+			$container = $block->getLayout()->getBlock('additional.product.info');
+			$popup = $block->getLayout()->createBlock(SSE_EditCustomOptions_Block_Popup::ALIAS, 'additional.product.info.customoptions', array('template' => 'sse/editcustomoptions/popup.phtml'));
+			$container->append($popup, self::POPUP_ALIAS_IN_LAYOUT);
+		}
+	}
+	/**
+	 * Remove popup block after rendering, so that following renderers can use their own instance
+	 * 
+	 * @param Varien_Event_Observer $observer
+	 */
+	public function removePopupBlockFromLayout(Varien_Event_Observer $observer)
+	{
+		$block = $observer->getBlock();
+		if ($block instanceof Mage_Sales_Block_Order_Item_Renderer_Default) {
+			/* @var $container Mage_Core_Block_Text_List */
+			$container = $block->getLayout()->getBlock('additional.product.info');
+			$container->unsetChild(self::POPUP_ALIAS_IN_LAYOUT);
+		}
+	}
+}
